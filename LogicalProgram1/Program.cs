69da3b49a384cc20d001327766abc75458e7c7ad
Hemyani <hdg2512@gmail.com>
2022-11-25 14:46:01 +0530

    Add project files.

---
+++ LogicalProgram1/Program.cs
@@ -0,0 +1,28 @@
+﻿using LogicalProgram1;
+using System;
+namespace LogicalProgram1
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FibonacciSeries.Fibonacci();
+            //Console.ReadLine();
+
+            //PerfectNumber.PerfectNo();
+            //Console.ReadLine();
+
+            //PrimeNumber.PrimeNo();
+            //Console.ReadLine();
+
+            //ReverseNumber.ReverseNo();
+            //Console.ReadLine();
+
+            //CouponNumbers.CouponNo();
+            //Console.ReadLine();
+
+            SimulateStopwatch.Stopwatch();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalProgram1/ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram1
+{
+    internal class ReverseNumber
+    {
+        public static void ReverseNo()
+        {
+            //variables
+            int n, reverse = 0;
+            Console.WriteLine("Enter  the  number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                //reverse Calculation
+                reverse = reverse * 10;
+                reverse = reverse + n % 10;
+                n = n / 10;
+            }
+            //print the reverse number
+            Console.WriteLine("Reverse of Entered Number is : " + reverse);
+            Console.ReadLine();
+        }
+    }
+}
